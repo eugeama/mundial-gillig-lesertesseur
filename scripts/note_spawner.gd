eff5a9882328e6557ca8384e0fd9c44fdfe272b2
Eugeles <eufrales@gmail.com>
2026-05-12 14:31:29 -0300

    las notas aparecen en momentos y lanes random

---
+++ scripts/note_spawner.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@export var noteScene: PackedScene
+@onready var spawnTimer = $spawnTimer
+
+const LANES={
+	1: 200,
+	2: 400,
+	3: 600,
+	4: 800
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	spawnTimer.wait_time = randf_range(0.5, 1.2)
+	spawnTimer.timeout.connect(spawnNote)
+	spawnTimer.start()
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func spawnNote():
+	var lane = chooseLane()
+	var newNote = noteScene.instantiate()
+	newNote.position.x = LANES[lane]
+	add_child(newNote)
+	spawnTimer.wait_time = randf_range(0.5, 1.2)
+	spawnTimer.start()
+	
+func chooseLane():
+	var lane = randi_range(1,4)
+	return lane
